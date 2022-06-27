60aeee9a7a3bcf3c7fa835f70b7521f591871cd7
Matheus leão <kyracraft0@gmail.com>
2022-06-27 15:23:51 -0300

    [Requisito 3] criado header da página

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <h1>TrybeWallet</h1>
+          <h2 data-testid="email-field">
+            {`User: ${email}`}
+          </h2>
+          <h3 data-testid="total-field">0</h3>
+          <h3 data-testid="header-currency-field">BRL</h3>
+        </header>
+
+      </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: propTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
