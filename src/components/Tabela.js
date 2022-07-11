1bc03c56fd3115c0c6e6bbec8065bb0121f5f918
Matheus leão <kyracraft0@gmail.com>
2022-07-11 02:33:16 -0300

    [Requisito 8] criada tabela com informações do state

--- src/components/Tabela.js
+++ src/components/Tabela.js
@@ -1,38 +1,69 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Tabela extends Component {
-    handleClick = ({ target }) => {
-      target.remove();
-    }
-
-    render() {
+  getTotalField = () => {
+    const { expenses } = this.props;
+    if (expenses) {
       return (
+        expenses.map((info, index) => (
+          <tr key={ index }>
+            <td>{info.description}</td>
+            <td>{info.tag}</td>
+            <td>{info.method}</td>
+            <td>
+              {info.value}
+              .00
+            </td>
+            <td>{info.currency}</td>
+            <td>{info.exchangeRates[info.currency].name.split('/Real Brasileiro')}</td>
+            <td>{parseFloat(info.exchangeRates[info.currency].ask).toFixed(2)}</td>
+            <td>
+              {
+                parseFloat(info.exchangeRates[info.currency].ask * info.value).toFixed(2)
+              }
 
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-            {/* <button
-              data-testid="delete-btn"
-              type="button"
-              onClick={ this.handleClick }
-            >
-              Excluir
-
-            </button> */}
+            </td>
+            <td>Real</td>
           </tr>
-
-        </thead>
+        ))
 
       );
     }
+  }
+
+  render() {
+    const { expenses } = this.props;
+    return (
+
+      <thead>
+
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        {expenses && this.getTotalField()}
+
+      </thead>
+
+    );
+  }
 }
 
-export default Tabela;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Tabela.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps)(Tabela);
