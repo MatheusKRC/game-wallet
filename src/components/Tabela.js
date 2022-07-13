7ba967a21e5baf704908b246516648ea877b4b9f
Matheus leão <kyracraft0@gmail.com>
2022-07-13 00:31:12 -0300

    correção

--- src/components/Tabela.js
+++ src/components/Tabela.js
@@ -52,15 +52,15 @@ class Tabela extends Component {
 
         <thead>
           <th>
-            <td>Descrição</td>
-            <td>Tag</td>
-            <td>Método de pagamento</td>
-            <td>Valor</td>
-            <td>Moeda</td>
-            <td>Câmbio utilizado</td>
-            <td>Valor convertido</td>
-            <td>Moeda de conversão</td>
-            <td>Editar/Excluir</td>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </th>
 
           {expenses && this.getTotalField()}
