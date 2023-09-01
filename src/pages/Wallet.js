5b1c722d8ca1f403808f050daed5672b13f9e7e1
Matheus leão <kyracraft0@gmail.com>
2023-09-01 00:58:10 -0300

    Começo do css da página de despesas

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,7 +6,7 @@ import Header from '../components/Header';
 class Wallet extends React.Component {
   render() {
     return (
-      <div>
+      <div className="walletPage">
         <Header />
         <Forms />
         <Tabela />
